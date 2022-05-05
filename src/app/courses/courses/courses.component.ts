5d2e1c91831472e53c5d0aaee9b24112df60285b
Rodrigo Marinho <rodrigo@pontta.com>
2022-05-04 22:07:08 -0300

    Spinner

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent implements OnInit {
+  courses: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+  }
+
+  ngOnInit(): void {}
+}
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Course } from '../model/course';
+import { take, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) {}
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      take(1),
+      tap(courses => console.log(courses))
+    );
+  }
+}
